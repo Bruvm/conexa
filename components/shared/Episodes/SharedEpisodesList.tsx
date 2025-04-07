e48f15a863a25425a93df64db712439153902188
MeliBruvera <62814445+Bruvm@users.noreply.github.com>
2025-04-07 15:23:24 -0300

    change

--- components/shared/Episodes/SharedEpisodesList.tsx
+++ components/shared/Episodes/SharedEpisodesList.tsx
@@ -24,8 +24,7 @@ export interface Episode {
 export const SharedEpisodeList = ({ characterOne, characterTwo }: SharedEpisodeProps) => {
     const [sharedEpisodes, setSharedEpisodes] = useState([]);
     const [loading, setLoading] = useState<boolean>(true);
-    
-    /* eslint-disable react-hooks/exhaustive-deps */
+
     useEffect(() => {
         getSharedEpisodes();
     }, [characterOne, characterTwo]);
